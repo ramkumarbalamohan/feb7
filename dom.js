91e3030e567044c08d76e8530d39cae7a8317866
ramkumarbalamohan <ramkumarbtech95@gmail.com>
2024-02-07 11:31:30 +0530

    feb commit

---
+++ dom.js
@@ -0,0 +1,85 @@
+// function additon(a) {
+//     console.log(a);
+// }
+// additon(20);
+
+// let a=function(b,c,d,e){
+// console.log(b,c,d,e);
+// }
+// a(30,40,50,60);
+
+
+// x=10; y=10
+
+// console.log(x+y);
+
+
+// function ad(x,y){
+// console.log(x+y);
+// }
+// ad(10,10);
+
+
+// let one = document.getElementById("h1");
+// console.log();
+
+// document.getElementById("tt").innerHTML = "Hello every one";
+// document.getElementById
+
+// document.getElementById("A").style.color ="blue";
+
+
+// function name(params) {
+    
+// }
+// name();
+
+
+// let value=function(){
+
+// }
+// value();
+
+// let b=()=>{
+
+// }
+// b();
+document.getElementById("id").style.color = "blue";
+document.getElementById("B").style.backgroundColor="red";
+document.getElementById("B").style.fontSize="large"
+document.getElementById("E").style.fontFamily="times new roman"
+
+let three=["hello","hai",12,15]
+three.map((v,a,i)=>{
+
+console.log(v);
+
+});
+let value={
+    persons:{
+student1:"ram",
+student2:"ramk",
+student3:"ramku",
+student4:"ramkumar",
+    }
+}
+console.log(value.persons.student4);
+
+let data=function(){
+
+}
+data();
+
+function na(params) {
+    
+}
+na();
+
+let c=()=>{
+
+}
+c();
+
+<button type="button" 
+onclick="document.getElementById('check').style.color = 'red'">
+Click Me!</button>
